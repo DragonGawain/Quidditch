71d4bc5fa0ac91c7714059b29334179ceae9ec0f
Craig <craigk00@hotmail.com>
2024-01-25 17:15:09 -0500

    Fixed the prototype

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     Inputs inputs;
-    Vector3 rotationInputs = new Vector3(0,0,0);
+    Vector3 rotationInputs = new Vector3(0, 0, 0);
     float movementInput = 0;
     Rigidbody body;
-
 
     // Start is called before the first frame update
     void Awake()
@@ -23,31 +22,35 @@ public class CharacterController : MonoBehaviour
     {
         rotationInputs = inputs.Player.Rotate.ReadValue<Vector3>();
         if (rotationInputs.x > 0)
-            transform.Rotate(1,0,0);
+            transform.Rotate(1, 0, 0);
         else if (rotationInputs.x < 0)
-            transform.Rotate(-1,0,0);
-        
+            transform.Rotate(-1, 0, 0);
+
         if (rotationInputs.y > 0)
-            transform.Rotate(0,1,0);
+            transform.Rotate(0, 1, 0);
         else if (rotationInputs.y < 0)
-            transform.Rotate(0,-1,0);
+            transform.Rotate(0, -1, 0);
 
         if (rotationInputs.z > 0)
-            transform.Rotate(0,0,1);
+            transform.Rotate(0, 0, 1);
         else if (rotationInputs.z < 0)
-            transform.Rotate(0,0,-1);
+            transform.Rotate(0, 0, -1);
 
         movementInput = inputs.Player.Move.ReadValue<float>();
         if (movementInput > 0)
         {
-            body.velocity += new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+            // body.velocity += new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+            Vector3 movementVector = transform.GetChild(0).position - transform.position;
+            movementVector.Normalize();
+            movementVector = movementVector / 2;
+            body.velocity += movementVector;
             body.velocity = Vector3.ClampMagnitude(body.velocity, 2);
         }
-        else if(movementInput < 0)
+        else if (movementInput < 0)
         {
             Vector3 deccel = new Vector3(body.velocity.x, body.velocity.y, body.velocity.z);
             deccel.Normalize();
-            deccel = deccel/2;
+            deccel = deccel / 2;
             body.velocity -= deccel;
         }
     }
