0f5f4fe6e9dfc92e364ebda5f1332891586c94db
Cristian Tesa <tesa.cristian@gmail.com>
2024-04-08 14:26:47 -0400

    Score UI
    - Score UI updates based on the in game outcome

--- Assets/Scripts/GameManagement/ScoreManager.cs
+++ Assets/Scripts/GameManagement/ScoreManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     private Dictionary<Team, int> teamScores = new Dictionary<Team, int>();
+
+    // Get both teams score text from the HUD
+    [SerializeField] TextMeshProUGUI team1Score;
+    [SerializeField] TextMeshProUGUI team2Score;
 
     private void Start()
     {
@@ -19,5 +24,9 @@ public class ScoreManager : MonoBehaviour
         teamScores[team]++;
         Debug.Log("Goal scored by " + team.ToString());
         Debug.Log("New score: Team 1 (Player): " + teamScores[Team.PLAYER] + " vs Team 2 (AI): " + teamScores[Team.AI]);
+
+        // Update the score on the UI
+        team1Score.text = teamScores[Team.PLAYER].ToString();
+        team2Score.text = teamScores[Team.AI].ToString();
     }
 }
