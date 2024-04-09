a17633255b945b11459f5bb9adbc019b5833efb4
Craig <craigk00@hotmail.com>
2024-04-09 00:06:28 -0400

    autoformat trees

--- Assets/Scripts/AI/BehaviourTree/Trees/Tree_Beater.cs
+++ Assets/Scripts/AI/BehaviourTree/Trees/Tree_Beater.cs
@@ -12,46 +12,59 @@ namespace CharacterAI
         // METHODS
         override protected Node PlantTheTree(BehaviourTree self)
         {
-            Node rootNode = new NodeSelector(self,
-                                             new List<Node> {
-                                                 new NodeSequence( self,
-                                                     new List<Node> {
-                                                         new Node_IsClosestBludgerTooClose(this),
-                                                         new Node_IsClosestBludgerTargettingMe(this),
-                                                         new Node_SetClosestBludgerAsTarget(this),
-                                                         new Node_IsTargetBludgerWithinHitDistance(this),
-                                                         new Node_HitBludgerAwayFromSelf(this),
-                                                         new Node_Wait60Frames(this)}),
-                                                 new NodeSelector(self,
-                                                     new List<Node>
-                                                     {
-                                                         new NodeSequence(self,
-                                                            new List<Node>
-                                                            {
-                                                                // Am I playing agressively?
-                                                                // Determine enemy target
-                                                                new Node_SetClosestBludgerFromTargetAsTarget(this),
-                                                                new Node_SeekTargetBludger(this),
-                                                                // Orient bludger and target. Not sure if needed.
-                                                                new Node_IsTargetBludgerWithinHitDistance(this),
-                                                                new Node_HitBludgerTowardsTarget(this),
-                                                                new Node_Wait60Frames(this)
-                                                            }),
-                                                         new NodeSequence(self,
-                                                            new List<Node>
-                                                            {
-                                                                // Am I playing defensively?
-                                                                // Determine target to protect.
-                                                                new Node_SeekTarget(this),
-                                                                new Node_IsNearTarget(this),
-                                                                new Node_IsClosestBludgerTooClose(this),
-                                                                new Node_SeekClosestBludger(this),
-                                                                new Node_SetClosestBludgerAsTarget(this),
-                                                                new Node_HitBludgerAwayFromTarget(this),
-                                                                new Node_Wait60Frames(this)
-                                                            })
-                                                     })
-                                                 });
+            Node rootNode = new NodeSelector(
+                self,
+                new List<Node>
+                {
+                    new NodeSequence(
+                        self,
+                        new List<Node>
+                        {
+                            new Node_IsClosestBludgerTooClose(this),
+                            new Node_IsClosestBludgerTargettingMe(this),
+                            new Node_SetClosestBludgerAsTarget(this),
+                            new Node_IsTargetBludgerWithinHitDistance(this),
+                            new Node_HitBludgerAwayFromSelf(this),
+                            new Node_Wait60Frames(this)
+                        }
+                    ),
+                    new NodeSelector(
+                        self,
+                        new List<Node>
+                        {
+                            new NodeSequence(
+                                self,
+                                new List<Node>
+                                {
+                                    // Am I playing agressively?
+                                    // Determine enemy target
+                                    new Node_SetClosestBludgerFromTargetAsTarget(this),
+                                    new Node_SeekTargetBludger(this),
+                                    // Orient bludger and target. Not sure if needed.
+                                    new Node_IsTargetBludgerWithinHitDistance(this),
+                                    new Node_HitBludgerTowardsTarget(this),
+                                    new Node_Wait60Frames(this)
+                                }
+                            ),
+                            new NodeSequence(
+                                self,
+                                new List<Node>
+                                {
+                                    // Am I playing defensively?
+                                    // Determine target to protect.
+                                    new Node_SeekTarget(this),
+                                    new Node_IsNearTarget(this),
+                                    new Node_IsClosestBludgerTooClose(this),
+                                    new Node_SeekClosestBludger(this),
+                                    new Node_SetClosestBludgerAsTarget(this),
+                                    new Node_HitBludgerAwayFromTarget(this),
+                                    new Node_Wait60Frames(this)
+                                }
+                            )
+                        }
+                    )
+                }
+            );
             return rootNode;
         }
     }
--- Assets/Scripts/AI/BehaviourTree/Trees/Tree_Chaser.cs
+++ Assets/Scripts/AI/BehaviourTree/Trees/Tree_Chaser.cs
@@ -12,61 +12,82 @@ namespace CharacterAI
         // METHODS
         override protected Node PlantTheTree(BehaviourTree self)
         {
-            Node rootNode = new NodeSelector(self,
-                                             new List<Node> {
-                                                 new NodeSequence( self,
-                                                    new List<Node>
-                                                    {
-                                                        new Node_IsClosestBludgerTooClose(this),
-                                                        new Node_FleeClosestBludger(this)
-                                                    }),
-                                                 new NodeSelector(self,
-                                                    new List<Node>
-                                                    {
-                                                        new NodeSequence(self,
-                                                            new List<Node>
-                                                            {
-                                                                new Node_AmIHoldingQuaffle(this),
-                                                                new NodeSelector(self,
-                                                                    new List<Node>
-                                                                    {
-                                                                        new NodeSequence(self,
-                                                                            new List<Node>
-                                                                            {
-                                                                                // Is teammate in a good spot for a pass?
-                                                                                new Node_CheckTeammateForPass(this),
-                                                                                // Pass to teammate
-                                                                                new Node_PassQuaffleToTeammate(this)
-                                                                            }),
-                                                                        new NodeSequence(self,
-                                                                            new List<Node>
-                                                                            {
-                                                                                // TODO: determine best goalpost.
-                                                                                new Node_HasReachedEnemyGoalpost(this),
-                                                                                new Node_ThrowQuaffleAtGoalpost(this)
-                                                                            }),
-                                                                        new Node_SeekEnemyGoalpost(this)
-                                                                    })
-                                                            }),
-                                                        new NodeSequence(self,
-                                                            new List<Node>
-                                                            {
-                                                                new Node_IsTeammateHoldingQuaffle(this),
-                                                                new Node_SeekFormationSpot(this)
-                                                            }),
-                                                        new NodeSelector(self,
-                                                            new List<Node>
-                                                            {
-                                                                new NodeSequence(self,
-                                                                    new List<Node>
-                                                                    {
-                                                                        new Node_IsEnemyHoldingQuaffle(this),
-                                                                        // Seek the point between the enemy and their target
-                                                                    }),
-                                                                new Node_SeekQuaffle(this)
-                                                            })
-                                                    })
-                                             });
+            Node rootNode = new NodeSelector(
+                self,
+                new List<Node>
+                {
+                    new NodeSequence(
+                        self,
+                        new List<Node>
+                        {
+                            new Node_IsClosestBludgerTooClose(this),
+                            new Node_FleeClosestBludger(this)
+                        }
+                    ),
+                    new NodeSelector(
+                        self,
+                        new List<Node>
+                        {
+                            new NodeSequence(
+                                self,
+                                new List<Node>
+                                {
+                                    new Node_AmIHoldingQuaffle(this),
+                                    new NodeSelector(
+                                        self,
+                                        new List<Node>
+                                        {
+                                            new NodeSequence(
+                                                self,
+                                                new List<Node>
+                                                {
+                                                    // Is teammate in a good spot for a pass?
+                                                    new Node_CheckTeammateForPass(this),
+                                                    // Pass to teammate
+                                                    new Node_PassQuaffleToTeammate(this)
+                                                }
+                                            ),
+                                            new NodeSequence(
+                                                self,
+                                                new List<Node>
+                                                {
+                                                    // TODO: determine best goalpost.
+                                                    new Node_HasReachedEnemyGoalpost(this),
+                                                    new Node_ThrowQuaffleAtGoalpost(this)
+                                                }
+                                            ),
+                                            new Node_SeekEnemyGoalpost(this)
+                                        }
+                                    )
+                                }
+                            ),
+                            new NodeSequence(
+                                self,
+                                new List<Node>
+                                {
+                                    new Node_IsTeammateHoldingQuaffle(this),
+                                    new Node_SeekFormationSpot(this)
+                                }
+                            ),
+                            new NodeSelector(
+                                self,
+                                new List<Node>
+                                {
+                                    new NodeSequence(
+                                        self,
+                                        new List<Node>
+                                        {
+                                            new Node_IsEnemyHoldingQuaffle(this),
+                                            // Seek the point between the enemy and their target
+                                        }
+                                    ),
+                                    new Node_SeekQuaffle(this)
+                                }
+                            )
+                        }
+                    )
+                }
+            );
             return rootNode;
         }
     }
