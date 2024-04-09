bdbdd829d3d761ec2f1244f82e4b8ddfa736d01c
Craig <craigk00@hotmail.com>
2024-04-09 11:49:31 -0400

    fixed stuff

--- Assets/Scripts/GameManagement/ScoreManager.cs
+++ Assets/Scripts/GameManagement/ScoreManager.cs
@@ -8,11 +8,24 @@ public class ScoreManager : MonoBehaviour
     private Dictionary<Team, int> teamScores = new Dictionary<Team, int>();
 
     // Get both teams score text from the HUD
-    [SerializeField] TextMeshProUGUI team1Score;
-    [SerializeField] TextMeshProUGUI team2Score;
+    [SerializeField]
+    TextMeshProUGUI team1Score;
+
+    [SerializeField]
+    TextMeshProUGUI team2Score;
+
+    public static ScoreManager singleton = null;
 
     private void Start()
     {
+        if (singleton == null)
+            singleton = this;
+
+        if (singleton != this)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+
         // Initialize scores for each team
         teamScores[Team.PLAYER] = 0;
         teamScores[Team.AI] = 0;
@@ -23,7 +36,12 @@ public class ScoreManager : MonoBehaviour
         // Increment the score for the specified team
         teamScores[team]++;
         Debug.Log("Goal scored by " + team.ToString());
-        Debug.Log("New score: Team 1 (Player): " + teamScores[Team.PLAYER] + " vs Team 2 (AI): " + teamScores[Team.AI]);
+        Debug.Log(
+            "New score: Team 1 (Player): "
+                + teamScores[Team.PLAYER]
+                + " vs Team 2 (AI): "
+                + teamScores[Team.AI]
+        );
 
         // Update the score on the UI
         team1Score.text = teamScores[Team.PLAYER].ToString();
